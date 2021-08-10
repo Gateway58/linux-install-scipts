341dec970939179e15626dff52e5fe816b7b8466
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-08-10 13:22:42 +0200

    Update install.sh

--- pacman/install.sh
+++ pacman/install.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-sudo pacman -S libreoffice-fresh inkscape gimp git brave ttf-ubuntu-font-family ttf-roboto ttf-lato dpkg neofetch gdb gcc vim 
+sudo pacman -S libreoffice-fresh inkscape gimp git brave ttf-ubuntu-font-family ttf-roboto ttf-lato dpkg neofetch gdb gcc nvim code linux-lts
 sudo pacman -Syu
